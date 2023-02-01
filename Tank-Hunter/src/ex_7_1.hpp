77c3223c1ce8a430367b388673c58487378f026a
Krystian Osinski <osinski.krystian.1999@gmail.com>
2023-02-01 19:52:34 +0100

    Add flashlight animations

--- Tank-Hunter/src/ex_7_1.hpp
+++ Tank-Hunter/src/ex_7_1.hpp
@@ -19,7 +19,8 @@
 
 Core::RenderContext sphereContext;
 Core::RenderContext shipContext;
-Core::RenderContext RPG7;
+Core::RenderContext RPG7_Context;
+Core::RenderContext FlashlightContext;
 
 GLuint program;
 Core::Shader_Loader shaderLoader;
@@ -65,25 +66,38 @@ float returnStartTime = 0;
 float deltaTime2 = 0.f;
 
 //Zmiany w ekwipunku
-bool rpg7 = true;
-bool flashlight = false;
+bool rpg7 = false;
+bool flashlight = true;
+bool flashlight_hide = false;
+bool flashlight_takeOut = false;
+bool flashlight_takingOut = false;
+bool flashlight_hiding = false;
+bool rpg7_hide = false;
+bool rpg7_takeOut = false;
+bool rpg7_hiding = false;
+bool flashlight_to_RPG7 = false;
+bool RPG7_to_flashlight = false;
+
+void getAnimationTime(float &StartTime,float delta2) {
+	if (StartTime <= 0) {
+		StartTime = glfwGetTime();
+		if (delta2 != 0) {
+			deltaTime2 = delta2;
+		}
+	}
+}
+
 
 void RPG_reload_animation(float currentTime) {
 	reloading = true;
 	
-	if (animStartTime <= 0) {
-		animStartTime = glfwGetTime();
-		
-	}
+	getAnimationTime(animStartTime, 0);
 	
 	deltaTime = currentTime - animStartTime;
 	if (deltaTime <= 3.5) {
 		if (deltaTime >= 1.75) {
 
-			if (returnStartTime <= 0) {
-				returnStartTime = glfwGetTime();
-				deltaTime2 = 1.75;
-			}
+			getAnimationTime(returnStartTime, 1.75);
 			deltaTime = deltaTime2 - (currentTime - returnStartTime);}
 	}
 	else {
@@ -95,19 +109,16 @@ void RPG_reload_animation(float currentTime) {
 	}
 }
 
+
+
 void RPG_aim_animation(float currentTime) {
 	animationAming = true;
-	if (animStartTime <= 0) {
-		animStartTime = glfwGetTime();
-	}
+	getAnimationTime(animStartTime, 0);
 	deltaTime = currentTime - animStartTime;
 
 	if (aiming) {
 		if (returnToHold) {
-			if (returnStartTime <= 0) {
-				returnStartTime = glfwGetTime();
-				deltaTime2 = 0.9;
-			}
+			getAnimationTime(returnStartTime, 0.9);
 
 			deltaTime = deltaTime2 - (currentTime - returnStartTime);
 			if (deltaTime <= 0) {
@@ -128,6 +139,75 @@ void RPG_aim_animation(float currentTime) {
 		
 	}
 }
+
+void RPG_takeOut(float currentTime) {
+	rpg7_takeOut = true;
+
+	getAnimationTime(animStartTime, 0);
+
+	deltaTime = currentTime - animStartTime;
+	if (deltaTime >= 2.25) {
+		
+		animStartTime = 0;
+		rpg7_hide = false;
+		rpg7_takeOut = false;
+		rpg7 = false;
+
+	}
+}
+
+void RPG_hide(float currentTime) {
+	rpg7_hiding = true;
+
+	getAnimationTime(animStartTime, 2.25);
+
+	deltaTime = deltaTime2 - (currentTime - animStartTime);
+	if (deltaTime <= 0) {
+		
+		animStartTime = 0;
+		rpg7_takeOut = false;
+		rpg7_takeOut = false;
+		rpg7_hiding = false;
+
+	}
+}
+
+
+void Flashlight_takeOut(float currentTime) {
+	flashlight_takingOut = true;
+
+	getAnimationTime(animStartTime, 1.2);
+
+	deltaTime = deltaTime2 - (currentTime - animStartTime);
+	if (deltaTime <= 0) {
+
+		animStartTime = 0;
+		flashlight_takeOut = false;
+		flashlight_takingOut = false;
+
+
+	}
+}
+
+void Flashlight_hide(float currentTime) {
+	flashlight_hiding = true;
+
+	getAnimationTime(animStartTime, 0);
+
+	deltaTime = currentTime - animStartTime;
+	if (deltaTime >= 1.2) {
+
+		animStartTime = 0;
+		flashlight_hiding = false;
+		flashlight = false;
+		flashlight_hide = false;
+
+	}
+}
+
+
+
+
 
 
 glm::mat4 createCameraMatrix()
@@ -205,7 +285,7 @@ void renderScene(GLFWwindow* window)
 	drawObjectColor(sphereContext, glm::eulerAngleY(currentTime / 3) * glm::translate(glm::vec3(4.f, 0, 0)) * glm::scale(glm::vec3(0.3f)) * glm::eulerAngleY(currentTime * 2), glm::vec3(1.f, 0, 0)); //0.3f
 
 	drawObjectColor(sphereContext,
-		glm::eulerAngleY(currentTime / 3) * glm::translate(glm::vec3(4.f, 0, 0)) * glm::eulerAngleY(currentTime) * glm::translate(glm::vec3(1.f, 0, 0)) * glm::scale(glm::vec3(0.1f)), glm::vec3(1.f, 0, 0));
+		glm::eulerAngleY(currentTime / 3) * glm::translate(glm::vec3(4.f, 0, 0)) * glm::eulerAngleY(currentTime) * glm::translate(glm::vec3(1.f, 0, 0)) * glm::scale(glm::vec3(0.1f)), glm::vec3(0, 0, 1.f));
 
 	glm::vec3 RPG7_Side = glm::normalize(glm::cross(RPG7_DIR, glm::vec3(0.f, 1.0f, 0.f)));
 	glm::vec3 RPG7_Up = glm::normalize(glm::cross(RPG7_Side, RPG7_DIR));
@@ -219,40 +299,71 @@ void renderScene(GLFWwindow* window)
 
 	
 	
-	
-		
-	if (hold) {
-		if (reload) {
-			//ANIMACJA PRZE£ADOWANIA RPG7
-			drawObjectColor(RPG7,
-				glm::translate(RPG7_POS) * (RPG7_Rotation_Matrix * glm::translate(glm::vec3(movementX, -0.07f + movementY, 0.05f+ movementZ)) ) * glm::eulerAngleXY(deltaTime / 1.2f, 1.44f) * glm::scale(glm::vec3(.0005f)),
-				glm::vec3(0.6f, 0.3f, 1.0f)
-			);
-			RPG_reload_animation(currentTime);
+	if (rpg7) {
 
+		if (hold) {
+			if (reload) {
+				//ANIMACJA PRZE£ADOWANIA RPG7_Context
+				drawObjectColor(RPG7_Context,
+					glm::translate(RPG7_POS) * (RPG7_Rotation_Matrix * glm::translate(glm::vec3(movementX, -0.07f + movementY, 0.05f + movementZ))) * glm::eulerAngleXY(deltaTime / 1.2f, 1.44f) * glm::scale(glm::vec3(.0005f)),
+					glm::vec3(0.6f, 0.3f, 1.0f)
+				);
+				RPG_reload_animation(currentTime);
+
+			}
+			else {
+				//ANIMACJA STANDAROWEGO PORUSZANIA SIÊ Z RPG7_Context
+					
+				if (rpg7_hide || rpg7_takeOut) {
+					drawObjectColor(RPG7_Context,
+						glm::translate(RPG7_POS) * (RPG7_Rotation_Matrix * glm::translate(glm::vec3(movementX, -0.07f + movementY, 0.05f + movementZ))) * glm::eulerAngleXY(deltaTime*3/4, 1.44f - deltaTime/2) * glm::scale(glm::vec3(.0005f)),
+						
+						glm::vec3(0.6f, 0.3f, 1.0f)
+					);
+					if (rpg7_hide) {
+						RPG_takeOut(currentTime);
+					}
+					else if (rpg7_takeOut){
+						RPG_hide(currentTime);
+					}
+				}
+				else {
+					drawObjectColor(RPG7_Context,
+						glm::translate(RPG7_POS) * (RPG7_Rotation_Matrix * glm::translate(glm::vec3(movementX, -0.07f + movementY, 0.05f + movementZ))) * glm::eulerAngleY(1.44f) * glm::scale(glm::vec3(.0005f)),// * glm::translate(glm::vec3(movementX, -0.07f+movementY, 0.05f + movementZ)))
+						glm::vec3(0.6f, 0.3f, 1.0f)
+					);
+
+				}
+				
+				
+			}
 		}
 		else {
-			//ANIMACJA STANDAROWEGO PORUSZANIA SIÊ Z RPG7
-			drawObjectColor(RPG7,
-				glm::translate(RPG7_POS) * (RPG7_Rotation_Matrix * glm::translate(glm::vec3(movementX, -0.07f+movementY, 0.05f + movementZ))) * glm::eulerAngleXY(+1.5f, 1.44f - 1.f) * glm::scale(glm::vec3(.0005f)),
-				//glm::translate(RPG7_POS) * (RPG7_Rotation_Matrix * glm::translate(glm::vec3(movementX, -0.07f + movementY, 0.05f + movementZ))) * glm::eulerAngleY(1.44f) * glm::scale(glm::vec3(.0005f)),// * glm::translate(glm::vec3(movementX, -0.07f+movementY, 0.05f + movementZ)))
+			//ANIMACJA CELOWANIA Z RPG7_Context
+			drawObjectColor(RPG7_Context,
+				glm::translate(RPG7_POS) * (RPG7_Rotation_Matrix * glm::translate(glm::vec3(0.0f - deltaTime / 9, -0.07f - deltaTime / 600, 0.05f - deltaTime / 45))) * glm::eulerAngleY(1.44f) * glm::scale(glm::vec3(.0005f)), //glm::pi<float>()   /**glm::translate(glm::vec3(-0.10f,-0.0715f,0.03f))*/
 				glm::vec3(0.6f, 0.3f, 1.0f)
 			);
+
+			if (!aiming || returnToHold) {
+				RPG_aim_animation(currentTime);
+			}
+
 		}
 	}
-	else {
-		//ANIMACJA CELOWANIA Z RPG7
-		drawObjectColor(RPG7,
-			glm::translate(RPG7_POS) * (RPG7_Rotation_Matrix * glm::translate(glm::vec3(0.0f - deltaTime / 9, -0.07f - deltaTime / 600, 0.05f - deltaTime / 45))) * glm::eulerAngleY(1.44f) * glm::scale(glm::vec3(.0005f)), //glm::pi<float>()   /**glm::translate(glm::vec3(-0.10f,-0.0715f,0.03f))*/
-			glm::vec3(0.6f, 0.3f, 1.0f)
+	else if (flashlight) {
+		drawObjectColor(FlashlightContext,
+			//glm::translate(RPG7_POS) * (RPG7_Rotation_Matrix * glm::translate(glm::vec3(movementX+0.12f, -0.135f + movementY, 0.05f + movementZ+0.12f))) * glm::eulerAngleY(1.44f)  * glm::scale(glm::vec3(.08f)),
+			glm::translate(RPG7_POS) * (RPG7_Rotation_Matrix * glm::translate(glm::vec3(movementX + 0.12f - deltaTime/17.143, -0.135f + movementY - deltaTime/14.1776, 0.05f + movementZ + 0.12f))) * glm::eulerAngleY(1.44f - deltaTime*144/120) * glm::scale(glm::vec3(.08f)),
+			glm::vec3(0.2f, 0.1f, 1.0f)
 		);
-
-		if (!aiming || returnToHold) {
-			RPG_aim_animation(currentTime);
+		if (flashlight_hide) {
+			Flashlight_hide(currentTime);
 		}
-
+		else if(flashlight_takeOut) {
+			Flashlight_takeOut(currentTime);
+		}
 	}
-		
 
 	glUseProgram(0);
 	glfwSwapBuffers(window);
@@ -287,7 +398,7 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 		
 		if (rpg7) {
 			
-			if (!reload) {
+			if (!reload && !rpg7_takeOut) {
 				if (aiming) {
 					returnToHold = true;
 				}
@@ -306,11 +417,14 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 		if (rpg7) {
 
 			if (hold) {
-				if (!reloading) {
+				if (!reloading && !rpg7_takeOut) {
 					reload = !reload;
 				}
 			}
 
+		}
+		else if (flashlight) {
+			
 		}
 
 	}
@@ -332,7 +446,8 @@ void init(GLFWwindow* window)
 
 	loadModelToContext("./models/sphere.obj", sphereContext);
 	loadModelToContext("./models/spaceship.obj", shipContext);
-	loadModelToContext("./models/Equipment/Rpg7.obj", RPG7);
+	loadModelToContext("./models/Equipment/Rpg7.obj", RPG7_Context);
+	loadModelToContext("./models/Equipment/Flashlight.obj", FlashlightContext);
 }
 
 void shutdown(GLFWwindow* window)
@@ -401,6 +516,41 @@ void processInput(GLFWwindow* window)
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
 		cameraPos += glm::normalize(glm::cross(cameraDir, cameraUp)) * moveSpeed;
 		moveXYZ = true;
+	}
+	if ((glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS) || RPG7_to_flashlight) {
+		if (!rpg7) {
+			if (!flashlight && !flashlight_hiding && !flashlight_takeOut) {
+				flashlight = true;
+				flashlight_takeOut = true;
+				RPG7_to_flashlight = false;
+				
+			}
+			else {
+				if (!flashlight_hiding && !flashlight_takeOut)
+					flashlight_hide = true;
+			}
+		}
+		else if (flashlight == false && !rpg7_hiding && !rpg7_takeOut && !aiming && !reloading && !animationAming) {
+			rpg7_hide = true;
+			RPG7_to_flashlight = true;
+		}
+	}
+	if ((glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) || flashlight_to_RPG7) {
+		if (!flashlight) {
+			if (rpg7 && !rpg7_takeOut && !aiming && !reloading && !animationAming && !rpg7_hiding ) {
+				rpg7_hide = !rpg7_hide;
+			}
+			else if (rpg7 == false && !rpg7_hiding && !rpg7_takeOut && !aiming && !reloading && !animationAming)  {
+				flashlight_to_RPG7 = false;
+				rpg7 = true;
+				rpg7_takeOut = !rpg7_takeOut;
+			}
+		}
+		else if(rpg7 == false && !flashlight_hiding && !flashlight_takeOut){
+			flashlight_hide = true;
+			flashlight_to_RPG7 = true;
+			
+		}
 	}
 	
 	if (moveXYZ) {
